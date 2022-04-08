e28c3a3346d4543ec434358da64e3ea30d1f143c
NYC0326 <71819307+NYC0326@users.noreply.github.com>
2022-04-08 16:55:20 +0900

    Create problem4.py

---
+++ NamYuchan/week2/problem4.py
@@ -0,0 +1,9 @@
+def solution(participant, completion):
+    answer = {}            
+    for i in participant:
+        answer[i] = answer.get(i, 0) + 1
+    for i in completion:
+        answer[i] -= 1
+    for i in answer:
+        if answer[i]:
+            return i
